b9b01bd9e1a0b588a1e302a56738b7a37fa98458
Radek Holý <radek.holy@applifting.cz>
2020-04-27 17:53:27 +0200

    WIP

--- link_shortener/tests/test_simple_views.py
+++ link_shortener/tests/test_simple_views.py
@@ -1,7 +1,14 @@
 import pytest
-from link_shortener.server import app
+from link_shortener.server import create_app
 
 
 def test_about_page():
+    app = create_app()
+    request, response = app.test_client.get('/links/about')
+    assert response.status == 200
+
+
+def test_about_page2():
+    app = create_app()
     request, response = app.test_client.get('/links/about')
     assert response.status == 200
